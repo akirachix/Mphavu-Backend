9030438585bc9404d2a19929027f85e59d9fef23
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-10-18 13:27:00 +0300

    made changes

--- performance/models.py
+++ performance/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+from teams.models import Player
+
 class Performance(models.Model):
     performance_id = models.AutoField(primary_key=True)
-    player_id = models.PositiveSmallIntegerField()
-    # player_id = models.ForeignKey(player, on_delete=models.CASCADE)
+    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
     passes = models.IntegerField()
     assists = models.IntegerField()
     no_of_goals = models.IntegerField()
